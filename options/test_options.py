a50bd5a794193b03393041958728715732fff145
1900zyh <zengyh7@mail2.sysu.edu.cn>
2021-12-12 12:20:08 +0900

    runnable

--- options/test_options.py
+++ options/test_options.py
@@ -4,7 +4,7 @@ class TestOptions(BaseOptions):
     def initialize(self):
         BaseOptions.initialize(self)
 
-        self.parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here')
+        self.parser.add_argument('--results_dir', type=str, default='gin_places2', help='saves results here')
         self.parser.add_argument('--phase', type=str, default='test', help='train or test')
         self.parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load?')              
 
