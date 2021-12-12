8323f274591a4c0bbbcb99a6058ede9c9bb1e7a1
1900zyh <zengyh7@mail2.sysu.edu.cn>
2021-12-12 12:35:23 +0900

    bugfix: test options

--- options/test_options.py
+++ options/test_options.py
@@ -12,7 +12,7 @@ class TestOptions(BaseOptions):
         self.parser.add_argument('--pretrained_path', type=str, default='./checkpoints/latest_net_G.pth')
         self.parser.add_argument('--level', type=int, required=True)
         self.parser.add_argument('--split', type=int, default=0)
-        self.parser.add_argument('--total', type=str, default=1)
+        self.parser.add_argument('--total', type=int, default=1)
 
         self.isTrain = False
 
